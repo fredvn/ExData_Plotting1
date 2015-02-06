3526786e0f2431b1ff04b84c9320d037b0e02731
fredvn <fred.van.nimwegen@gmail.com>
2015-02-06 15:26:45 +0100

    final version

--- plot2.R
+++ plot2.R
@@ -12,8 +12,8 @@ setInternet2(use = TRUE)
 # link to the archive containing the project data
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 # download the archive
-##download.file(fileUrl, destfile = "./data/power_consumption.zip")
-##unzip("./data/power_consumption.zip",exdir="./data")
+download.file(fileUrl, destfile = "./data/power_consumption.zip")
+unzip("./data/power_consumption.zip",exdir="./data")
 
 hpc <- read.csv("./data/household_power_consumption.txt",sep=";",na.strings=c("?"),stringsAsFactors=FALSE)
 newnames <- tolower(names(hpc))
